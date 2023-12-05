082831f89cf3bc696222560f291d008b53e850ef
Emile Fokkema <emile.fokkema@navara.nl>
2023-12-05 15:47:22 +0100

    class

--- treinen.py
+++ treinen.py
@@ -1,33 +1,51 @@
 import json
 
-def choose_train(number, train, all_trains):
-    print(number)
-    length = 0
-    chosen_trains = []
-    next_trains = []
-    if train is None:
-        length = 0
-        chosen_trains = []
-        next_trains = all_trains
-    else:
-        length = train[1] - train[0]
-        chosen_trains = [train]
-        next_trains = [next_train for next_train in all_trains if next_train[0] >= train[1]]
-    next_trains_length = 0
-    next_chosen_trains = []
-    for next_train in next_trains:
-        (candidate_length, candidate_chosen_trains) = choose_train(number + 1, next_train, all_trains)
-        if candidate_length >= next_trains_length:
-            next_trains_length = candidate_length
-            next_chosen_trains = candidate_chosen_trains
-        #else:
-            #print(f'skipping candidate for train {number + 1}')
-    return (length + next_trains_length, chosen_trains + next_chosen_trains)
+def calculate(trains):
+    if len(trains) == 0:
+        return (0, [])
+    best_length = 0
+    best_trains = []
+    for train in trains:
+        (length, trains) = train.determine_best_next_trains(trains)
+        candidate_length = best_length
+
+class Train:
+
+    def __init__(self, index, times):
+        self.index = index
+        self.times = times
+        self.best_next_trains = None
+    
+    @property
+    def start(self):
+        return self.times[0]
+    
+    @property
+    def end(self):
+        return self.times[1]
+    
+    @property
+    def length(self):
+        return self.end - self.start
+    
+    def __str__(self) -> str:
+        return f'{self.start} -> {self.end}'
+    
+    def determine_best_next_trains(self, all_trains):
+        if self.best_next_trains is not None:
+            return self.best_next_trains
+        print('calculating best next trains for train', self.index)
+        # next_trains = [next for next in all_trains if next.start >= self.end]
+        # if len(next_trains) == 0:
+        #     return
+        # best_next_length = 0
+        # best_next_trains = []
+        # for next_train in next_trains:
+
+
 
 with open('input.json') as inputJson:
-    input = json.load(inputJson)
-    all_trains = input
-    print('total number of trains:', len(all_trains))
-    (length, chosen_trains) = choose_train(0, None, all_trains)
-    print('max length:', length)
-    print(f'using {len(chosen_trains)} trains', chosen_trains)
+    input = json.load(inputJson)[0:3]
+    all_trains = [Train(index, times) for index, times in enumerate(input)]
+    all_trains[0].determine_best_next_trains(all_trains)
+    
