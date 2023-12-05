4353321bb0a4684b9227ac89e90ed4142e41aba7
Emile Fokkema <emile.fokkema@navara.nl>
2023-12-05 16:20:19 +0100

    better

--- treinen.py
+++ treinen.py
@@ -7,7 +7,12 @@ def calculate(trains):
     best_trains = []
     for train in trains:
         (length, trains) = train.determine_best_next_trains(trains)
-        candidate_length = best_length
+        if length >= best_length:
+            best_length = length
+            best_trains = trains
+        
+    return (best_length, best_trains)
+    
 
 class Train:
 
@@ -29,23 +34,26 @@ class Train:
         return self.end - self.start
     
     def __str__(self) -> str:
-        return f'{self.start} -> {self.end}'
+        return f'[{self.index}]({self.start} -> {self.end})'
     
     def determine_best_next_trains(self, all_trains):
         if self.best_next_trains is not None:
             return self.best_next_trains
         print('calculating best next trains for train', self.index)
-        # next_trains = [next for next in all_trains if next.start >= self.end]
-        # if len(next_trains) == 0:
-        #     return
-        # best_next_length = 0
-        # best_next_trains = []
-        # for next_train in next_trains:
+        next_trains = [next for next in all_trains if next.start >= self.end]
+        (next_length, best_next_trains) = calculate(next_trains)
+        result = (self.length + next_length, [self] + best_next_trains)
+        self.best_next_trains = result
+        print('done calculating best next trains for train', self.index)
+        return result
 
 
 
 with open('input.json') as inputJson:
-    input = json.load(inputJson)[0:3]
+    input = json.load(inputJson)
     all_trains = [Train(index, times) for index, times in enumerate(input)]
-    all_trains[0].determine_best_next_trains(all_trains)
+    (length, trains) = calculate(all_trains)
+    print('final length ', length)
+    for train in trains:
+        print(str(train))
     
